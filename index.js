56bbd457c24090b805d0b1ca03e792c8da5f2903
KarepeN <s540109@nwmissouri.edu>
2020-10-09 22:34:21 -0500

    updated html

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello to the World Beyond. This is Nithya Karepe trying to make contact with other humans.')
+  res.sendFile(path.join(__dirname + '/index.html'));
+ // res.send('Hello to the World Beyond. This is Nithya Karepe trying to make contact with other humans.')
 })
 
 app.listen(port, () => {
